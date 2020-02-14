d92e128a5eb238fd7dd3a6e2a05cddc621180753
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2020-02-14 17:08:58 +0100

    added value parsing and some test, changed strings to slices

--- src/parser.rs
+++ src/parser.rs
@@ -38,7 +38,7 @@ pub mod util {
         }
     }
 
-    pub fn get_text_pos(position: usize, text: &String) -> String {
+    pub fn get_text_pos(position: usize, text: &str) -> String {
         let mut line = 1;
         let mut col = 1;
         let mut count = 0;
@@ -55,6 +55,16 @@ pub mod util {
             count += 1;
         }
         format!("ln {} col {}", line, col)
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::get_text_pos;
+        #[test]
+        fn test_util_get_text_pos() {
+            let text: String = "fun main(\n) test123".to_string();
+            assert_eq!(get_text_pos(15, &text), "ln 2 col 6".to_string());
+        }
     }
 }
 
@@ -219,7 +229,7 @@ pub mod lexer {
 
     fn get_number_token(
         iterator: &mut Peekable<Enumerate<Chars>>,
-        text: &String,
+        text: &str,
     ) -> Result<Token, ParserError> {
         let position = get_char(iterator)?.0;
         let mut current_token: String = String::new();
@@ -255,7 +265,7 @@ pub mod lexer {
 
     fn get_string_token(
         iterator: &mut Peekable<Enumerate<Chars>>,
-        text: &String,
+        text: &str,
     ) -> Result<Token, ParserError> {
         let position = get_char(iterator)?.0;
         iterator.next();
@@ -291,7 +301,7 @@ pub mod lexer {
 
     pub fn get_token(
         iterator: &mut Peekable<Enumerate<Chars>>,
-        text: &String,
+        text: &str,
     ) -> Result<Token, ParserError> {
         let (position, mut next) = get_char(iterator)?;
         if next.is_whitespace() {
@@ -450,6 +460,97 @@ pub mod lexer {
             }
         }
     }
+
+    #[cfg(test)]
+    mod test {
+        use super::get_token;
+        use super::Token;
+        use std::fs;
+        use std::io;
+
+        fn run_lexer(contents: &str) -> Token {
+            let mut iterator = contents.chars().enumerate().peekable();
+
+            match get_token(&mut iterator, &contents.to_string()) {
+                Ok(t) => t,
+                Err(err) => panic!("{}", err),
+            }
+        }
+
+        #[test]
+        fn test_lexer_examples_no_panic() -> Result<(), io::Error> {
+            for ex in fs::read_dir("examples")? {
+                let path = ex?.path();
+                println!("{:?}", path);
+                let contents = fs::read_to_string(path)?;
+                run_lexer(contents.as_str());
+            }
+            Ok(())
+        }
+
+        #[test]
+        fn test_lexer_tokens() {
+            assert_eq!(
+                run_lexer("ßuperĸööl"),
+                Token::Ident(0, "ßuperĸööl".to_string()),
+                "Ident"
+            );
+            assert_eq!(
+                run_lexer("\"\\\"ĸthis\nis\r\nan interesting \\\"test\\\\ yeäöüöäöĸ\""),
+                Token::ValString(
+                    0,
+                    "\"ĸthis\nis\r\nan interesting \"test\\ yeäöüöäöĸ".to_string()
+                ),
+                "ValString"
+            );
+            assert_eq!(run_lexer("7435971"), Token::ValInt(0, 7435971), "ValInt");
+            assert_eq!(run_lexer("24.861"), Token::ValFloat(0, 24.861), "ValFloat");
+            assert_eq!(run_lexer("fun"), Token::Fun(0), "Fun");
+            assert_eq!(run_lexer("if"), Token::If(0), "If");
+            assert_eq!(run_lexer("else"), Token::Else(0), "Else");
+            assert_eq!(run_lexer("elif"), Token::Elif(0), "Elif");
+            assert_eq!(run_lexer("while"), Token::While(0), "While");
+            assert_eq!(run_lexer("continue"), Token::Continue(0), "Continue");
+            assert_eq!(run_lexer("break"), Token::Break(0), "Break");
+            assert_eq!(run_lexer("for"), Token::For(0), "For");
+            assert_eq!(run_lexer("in"), Token::In(0), "In");
+            assert_eq!(run_lexer("return"), Token::Return(0), "Return");
+            assert_eq!(run_lexer("and"), Token::BoolAnd(0), "BoolAnd");
+            assert_eq!(run_lexer("or"), Token::BoolOr(0), "BoolOr");
+            assert_eq!(run_lexer("true"), Token::ValTrue(0), "ValTrue");
+            assert_eq!(run_lexer("false"), Token::ValFalse(0), "ValFalse");
+            assert_eq!(run_lexer("none"), Token::ValNone(0), "None");
+            assert_eq!(run_lexer("("), Token::LPar(0), "LPar");
+            assert_eq!(run_lexer(")"), Token::RPar(0), "RPar");
+            assert_eq!(run_lexer("["), Token::LBrack(0), "LBrack");
+            assert_eq!(run_lexer("]"), Token::RBrack(0), "RBrack");
+            assert_eq!(run_lexer("{"), Token::LBrace(0), "LBrace");
+            assert_eq!(run_lexer("}"), Token::RBrace(0), "RBrace");
+            assert_eq!(run_lexer(";"), Token::Semi(0), "Semi");
+            assert_eq!(run_lexer(","), Token::Comma(0), "Comma");
+            assert_eq!(run_lexer("+"), Token::Add(0), "Add");
+            assert_eq!(run_lexer("-"), Token::Minus(0), "Minus");
+            assert_eq!(run_lexer("*"), Token::Mul(0), "Mul");
+            assert_eq!(run_lexer("%"), Token::Mod(0), "Mod");
+            assert_eq!(run_lexer("|"), Token::BitOr(0), "BitOr");
+            assert_eq!(run_lexer("^"), Token::BitXOr(0), "BitXOr");
+            assert_eq!(run_lexer("&"), Token::BitAnd(0), "BitAnd");
+            assert_eq!(run_lexer("<<"), Token::BitLsh(0), "BitLsh");
+            assert_eq!(run_lexer(">>"), Token::BitRsh(0), "BitRsh");
+            assert_eq!(run_lexer(">>>"), Token::BitURsh(0), "BitURsh");
+            assert_eq!(run_lexer("$"), Token::Concat(0), "Concat");
+            assert_eq!(run_lexer("//"), Token::IntDiv(0), "IntDiv");
+            assert_eq!(run_lexer("/"), Token::FloatDiv(0), "FloatDiv");
+            assert_eq!(run_lexer("="), Token::Assign(0), "Assign");
+            assert_eq!(run_lexer("=="), Token::Equals(0), "Equals");
+            assert_eq!(run_lexer("!="), Token::NotEquals(0), "NotEquals");
+            assert_eq!(run_lexer("!"), Token::BoolNot(0), "BoolNot");
+            assert_eq!(run_lexer("<"), Token::LessThan(0), "LessThan");
+            assert_eq!(run_lexer("<="), Token::LessEquals(0), "LessEquals");
+            assert_eq!(run_lexer(">"), Token::GreaterThan(0), "GreaterThan");
+            assert_eq!(run_lexer(">="), Token::GreaterEquals(0), "GreaterEquals");
+        }
+    }
 }
 
 pub mod parser {
@@ -463,7 +564,7 @@ pub mod parser {
 
     struct Parser<'a> {
         iterator: &'a mut Peekable<Enumerate<Chars<'a>>>,
-        contents: &'a String,
+        contents: &'a str,
         current: &'a mut Token,
     }
 
@@ -473,6 +574,7 @@ pub mod parser {
 
     enum Expression {
         FuncDef(Vec<Token>, Statement),
+        Value(Token),
     }
 
     enum Statement {
@@ -555,7 +657,24 @@ pub mod parser {
         }
     }
 
-    pub fn parse(contents: &String) -> Result<(), ParserError> {
+    fn parse_ex_value(parser: &mut Parser) -> Result<Expression, ParserError> {
+        if parser.accept(&Token::ValInt(0, 0))
+            || parser.accept(&Token::ValFloat(0, 0.0))
+            || parser.accept(&Token::ValNone(0))
+            || parser.accept(&Token::ValFalse(0))
+            || parser.accept(&Token::ValTrue(0))
+            || parser.accept(&Token::ValString(0, String::new()))
+            || parser.accept(&Token::Ident(0, String::new()))
+        {
+            let tok = parser.peek();
+            parser.eat()?;
+            Ok(Expression::Value(tok))
+        } else {
+            Err(ParserError::NotAccepted)
+        }
+    }
+
+    pub fn parse(contents: &str) -> Result<(), ParserError> {
         let mut iterator = contents.chars().enumerate().peekable();
         let mut token = lexer::get_token(&mut iterator, contents)?;
         let mut parser = Parser {
@@ -566,102 +685,35 @@ pub mod parser {
         parse_ex_func_def(&mut parser)?;
         Ok(())
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::lexer;
-    use super::lexer::Token;
-    use super::util;
-    use std::fs;
-    use std::io;
+    #[cfg(test)]
+    mod test {
+        use super::parse_ex_func_def;
+        use super::Expression;
+        use super::Parser;
+        use crate::parser::lexer;
+        use crate::parser::util::ParserError;
 
-    fn run_lexer(contents: &str) -> Token {
-        let mut iterator = contents.chars().enumerate().peekable();
-
-        match lexer::get_token(&mut iterator, &contents.to_string()) {
-            Ok(t) => t,
-            Err(err) => panic!("{}", err),
+        fn run_parser(
+            contents: &str,
+            fun: &dyn Fn(&mut Parser) -> Result<Expression, ParserError>,
+        ) {
+            let mut iterator = contents.chars().enumerate().peekable();
+            let mut token =
+                lexer::get_token(&mut iterator, contents).expect("couldnt read first token");
+            let mut parser = Parser {
+                iterator: &mut iterator,
+                contents: contents,
+                current: &mut token,
+            };
+            fun(&mut parser).expect("failed to parse");
         }
-    }
 
-    #[test]
-    fn test_util_get_text_pos() {
-        let text: String = "fun main(\n) test123".to_string();
-        assert_eq!(util::get_text_pos(15, &text), "ln 2 col 6".to_string());
-    }
-
-    #[test]
-    fn test_lexer_examples_no_panic() -> Result<(), io::Error> {
-        for ex in fs::read_dir("examples")? {
-            let path = ex?.path();
-            println!("{:?}", path);
-            let contents = fs::read_to_string(path)?;
-            run_lexer(contents.as_str());
+        #[test]
+        fn test_parser_func_def() {
+            run_parser("fun(test, args){}", &parse_ex_func_def);
+            run_parser("fun(test){}", &parse_ex_func_def);
+            run_parser("fun(){}", &parse_ex_func_def);
         }
-        Ok(())
-    }
-
-    #[test]
-    fn test_lexer_tokens() {
-        assert_eq!(
-            run_lexer("ßuperĸööl"),
-            Token::Ident(0, "ßuperĸööl".to_string()),
-            "Ident"
-        );
-        assert_eq!(
-            run_lexer("\"\\\"ĸthis\nis\r\nan interesting \\\"test\\\\ yeäöüöäöĸ\""),
-            Token::ValString(
-                0,
-                "\"ĸthis\nis\r\nan interesting \"test\\ yeäöüöäöĸ".to_string()
-            ),
-            "ValString"
-        );
-        assert_eq!(run_lexer("7435971"), Token::ValInt(0, 7435971), "ValInt");
-        assert_eq!(run_lexer("24.861"), Token::ValFloat(0, 24.861), "ValFloat");
-        assert_eq!(run_lexer("fun"), Token::Fun(0), "Fun");
-        assert_eq!(run_lexer("if"), Token::If(0), "If");
-        assert_eq!(run_lexer("else"), Token::Else(0), "Else");
-        assert_eq!(run_lexer("elif"), Token::Elif(0), "Elif");
-        assert_eq!(run_lexer("while"), Token::While(0), "While");
-        assert_eq!(run_lexer("continue"), Token::Continue(0), "Continue");
-        assert_eq!(run_lexer("break"), Token::Break(0), "Break");
-        assert_eq!(run_lexer("for"), Token::For(0), "For");
-        assert_eq!(run_lexer("in"), Token::In(0), "In");
-        assert_eq!(run_lexer("return"), Token::Return(0), "Return");
-        assert_eq!(run_lexer("and"), Token::BoolAnd(0), "BoolAnd");
-        assert_eq!(run_lexer("or"), Token::BoolOr(0), "BoolOr");
-        assert_eq!(run_lexer("true"), Token::ValTrue(0), "ValTrue");
-        assert_eq!(run_lexer("false"), Token::ValFalse(0), "ValFalse");
-        assert_eq!(run_lexer("none"), Token::ValNone(0), "None");
-        assert_eq!(run_lexer("("), Token::LPar(0), "LPar");
-        assert_eq!(run_lexer(")"), Token::RPar(0), "RPar");
-        assert_eq!(run_lexer("["), Token::LBrack(0), "LBrack");
-        assert_eq!(run_lexer("]"), Token::RBrack(0), "RBrack");
-        assert_eq!(run_lexer("{"), Token::LBrace(0), "LBrace");
-        assert_eq!(run_lexer("}"), Token::RBrace(0), "RBrace");
-        assert_eq!(run_lexer(";"), Token::Semi(0), "Semi");
-        assert_eq!(run_lexer(","), Token::Comma(0), "Comma");
-        assert_eq!(run_lexer("+"), Token::Add(0), "Add");
-        assert_eq!(run_lexer("-"), Token::Minus(0), "Minus");
-        assert_eq!(run_lexer("*"), Token::Mul(0), "Mul");
-        assert_eq!(run_lexer("%"), Token::Mod(0), "Mod");
-        assert_eq!(run_lexer("|"), Token::BitOr(0), "BitOr");
-        assert_eq!(run_lexer("^"), Token::BitXOr(0), "BitXOr");
-        assert_eq!(run_lexer("&"), Token::BitAnd(0), "BitAnd");
-        assert_eq!(run_lexer("<<"), Token::BitLsh(0), "BitLsh");
-        assert_eq!(run_lexer(">>"), Token::BitRsh(0), "BitRsh");
-        assert_eq!(run_lexer(">>>"), Token::BitURsh(0), "BitURsh");
-        assert_eq!(run_lexer("$"), Token::Concat(0), "Concat");
-        assert_eq!(run_lexer("//"), Token::IntDiv(0), "IntDiv");
-        assert_eq!(run_lexer("/"), Token::FloatDiv(0), "FloatDiv");
-        assert_eq!(run_lexer("="), Token::Assign(0), "Assign");
-        assert_eq!(run_lexer("=="), Token::Equals(0), "Equals");
-        assert_eq!(run_lexer("!="), Token::NotEquals(0), "NotEquals");
-        assert_eq!(run_lexer("!"), Token::BoolNot(0), "BoolNot");
-        assert_eq!(run_lexer("<"), Token::LessThan(0), "LessThan");
-        assert_eq!(run_lexer("<="), Token::LessEquals(0), "LessEquals");
-        assert_eq!(run_lexer(">"), Token::GreaterThan(0), "GreaterThan");
-        assert_eq!(run_lexer(">="), Token::GreaterEquals(0), "GreaterEquals");
     }
 }
